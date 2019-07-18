4846162cf04fbabb7624e1b42a73b106b8db9595
prasad <kandukuriprasadu@gmail.com>
2019-07-18 18:00:21 +0530

    test2

---
+++ GITTest/Test2ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  Test2ViewController.swift
+//  GITTest
+//
+//  Created by ypo on 18/07/19.
+//  Copyright © 2019 YPO. All rights reserved.
+//
+
+import UIKit
+
+class Test2ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("3")
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
